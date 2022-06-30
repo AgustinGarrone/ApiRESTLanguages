0096be586dbdd257ce38682ebf1dacf3d8244c05
Agustin Garrone <agustinngarrone@hotmail.com>
2022-06-30 09:39:51 -0300

    act index js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import app from "./app.js";
+import express from "express";
+import morgan from "morgan";
+// Routes
+import languageRoutes from "./routes/language.routes.js";
 
-const main=()=> {
-    app.listen(app.get("port"))
-    console.log(`Server on port ${app.get("port")}`)
-}
 
-main();
+const app=express();
+
+// Settings
+app.set("port",4000);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use("/api/V1/languages",languageRoutes);
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
